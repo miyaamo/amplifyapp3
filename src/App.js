275b8fb5859c7401c58aeb54304bb14bf771055c
miyaamo <u109246@gmail.com>
2022-08-06 17:36:19 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
-      <header>
-    <div>
+      </header>
+    </div>
   );
 }
 
